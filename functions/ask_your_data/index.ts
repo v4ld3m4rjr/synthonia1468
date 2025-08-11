1acc2441b026c9e92447b7d25fd383b76f8e92ce
v4ld3m4rjr <128973741+v4ld3m4rjr@users.noreply.github.com>
2025-08-11 19:37:53 -0300

    Add files via upload

--- functions/ask_your_data/index.ts
+++ functions/ask_your_data/index.ts
@@ -1,7 +1,4 @@
-// Edge Function: ask_your_data
-// Transforma intenção em template SQL seguro (whitelist) e retorna tabela + resumo.
 export default async (req: Request) => {
   const body = await req.json()
-  // TODO: mapear intents -> templates pré-aprovados
   return new Response(JSON.stringify({ ok: true, received: body }), { headers: { 'content-type': 'application/json' } })
 }
